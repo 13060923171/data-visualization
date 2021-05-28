79969ad19165dd17c8fcab709277e6908de87653
hao <960751327@qq.com>
2021-05-28 20:44:08 +0800

    生成可视化案例

--- 爬取京东商品属性/main.py
+++ 爬取京东商品属性/main.py
@@ -6,7 +6,6 @@ import re
 from tqdm import tqdm
 import time
 import random
-
 headers = {
 
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
